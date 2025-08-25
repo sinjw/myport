8a6f3f6b314983dfd9241964245e8c881746d2b1
신중원 <nadasjw@gmail.com>
2025-08-25 12:35:11 +0900

    fix

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { motion } from "framer-motion";
 import { AnimatedProjectModal } from "../components/portfolioModal/Modal";
 import { FloatingCircle } from "../components/circle";
